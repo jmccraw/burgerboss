f160584a247c460bf904a09d8c0efa02d44daac6
Justin McCraw <Justin.McCraw@nymag.com>
2014-09-18 13:05:30 -0400

    Removing Require.js

--- js/boot.js
+++ js/boot.js
@@ -1,37 +1,32 @@
-define([
-  "phaser.min"
-], function(Phaser) {
-  "use strict";
+"use strict";
 
-  /**
-    * Pre-load screen, boot sequence
-    */
-  var _BOOT = {
-    "preload": function() {
-      game.load.image("progress", "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAMgAAAAUAQMAAADyauCJAAAABGdBTUEAALGPC/xhBQAAAAFzUkdCAK7OHOkAAAADUExURf///6fEG8gAAAAPSURBVCjPY2AYBaOAgQEAAggAATy5/7MAAAAASUVORK5CYII=");
-      game.stage.backgroundColor = "#d7924a";
-      // check if on mobile and adjust accordingly
-      if (/\/m\//.test(window.location.href)) {
-        settings.global.mobile = true;
-        // scale to height, but if height would make width too large, base it on width instead
-        if ((window.innerHeight * (game.width / game.height) > 305) || (window.innerHeight >= 600)) {
-          game.scale.maxWidth = 305;
-          game.scale.maxHeight = game.scale.maxWidth * (game.height / game.width);
-        }
-        // else, scaled based on height
-        else {
-          game.scale.maxHeight = window.innerHeight;
-          game.scale.maxWidth = game.scale.maxHeight * (game.width / game.height);
-        }
-        game.scale.setSize();
-        console.log("_trackEvent", "Vulture, Video Game", "Game of Thrones", "Mobile");
-        // _gaq.push(["_trackEvent", "Vulture, Video Game", "Game of Thrones", "Mobile"]);
+/**
+  * Pre-load screen, boot sequence
+  */
+var _BOOT = {
+  "preload": function() {
+    game.load.image("progress", "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAMgAAAAUAQMAAADyauCJAAAABGdBTUEAALGPC/xhBQAAAAFzUkdCAK7OHOkAAAADUExURf///6fEG8gAAAAPSURBVCjPY2AYBaOAgQEAAggAATy5/7MAAAAASUVORK5CYII=");
+    game.stage.backgroundColor = "#d7924a";
+    // check if on mobile and adjust accordingly
+    if (/\/m\//.test(window.location.href)) {
+      settings.global.mobile = true;
+      // scale to height, but if height would make width too large, base it on width instead
+      if ((window.innerHeight * (game.width / game.height) > 305) || (window.innerHeight >= 600)) {
+        game.scale.maxWidth = 305;
+        game.scale.maxHeight = game.scale.maxWidth * (game.height / game.width);
       }
-    },
-
-    "create": function() {
-      game.state.start("load");
+      // else, scaled based on height
+      else {
+        game.scale.maxHeight = window.innerHeight;
+        game.scale.maxWidth = game.scale.maxHeight * (game.width / game.height);
+      }
+      game.scale.setSize();
+      console.log("_trackEvent", "Vulture, Video Game", "Game of Thrones", "Mobile");
+      // _gaq.push(["_trackEvent", "Vulture, Video Game", "Game of Thrones", "Mobile"]);
     }
-  };
+  },
 
-});
+  "create": function() {
+    game.state.start("load");
+  }
+};
